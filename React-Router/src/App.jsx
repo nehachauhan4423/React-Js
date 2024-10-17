fd9e2179e43c2e243169560b7b4c0f7b6191ada8
nehachauhan4423 <nehachauhan4423@gmail.com>
2024-10-17 11:14:56 +0530

    React

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom"
+import Router from "router"
+
+function App() {
+
+  return (
+   <BrowserRouter>
+   <Routers>
+
+   </Routers>
+   </BrowserRouter>
+  )
+}
+
+export default App
